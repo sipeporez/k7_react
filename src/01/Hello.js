3c281c73a98a1e81c2922ba6e246c56984a02cd6
sipeporez <sipeporez@gmail.com>
2024-05-02 17:50:00 +0900

    React

---
+++ src/01/Hello.js
@@ -0,0 +1,16 @@
+function Hello() {
+    return (
+        <>
+            <p>
+                Hello React
+            </p>
+            <ul>
+                <li>
+                    리액트 컴포넌트
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Hello;
