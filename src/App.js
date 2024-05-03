35baf7a64aca3efee970ab660b263e9f38871ca6
sipeporez <sipeporez@gmail.com>
2024-05-03 17:51:06 +0900

    First tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import Hello from './01/Hello';
+// import MyClock from './02/MyClock';
+import MyDiv from './03/MyDiv';
+import logo from './logo.svg'
+import { AiFillHome } from "react-icons/ai";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello />
-        <Hello />
-        <Hello />
+    <div className="flex flex-col w-full max-w-screen-xl mx-auto h-screen overscroll-y-auto">
+      <header className="flex justify-between items-center text-2xl font-bold p-10 bg-slate-200">
+        <div>리액트 실습</div>
+        <div><AiFillHome className='text-3xl text-zinc-700' /></div>
       </header>
+      <main className="grow">
+        <div className="flex justify-center items-center">
+          <img src={logo} className="App-logo" alt="logo"/>
+        </div>
+      </main>
+      <footer className="flex justify-center items-center text-white bg-slate-700 h-20">
+        ⓒ 2024 kimseongwoo. All rights reserved.
+      </footer>
     </div>
   );
 }
